f31564e9af0c4d890a0b5fa6956df08e00d2e9ee
GabrielFilomeno <biel_floripa@hotmail.com>
2024-05-17 18:17:01 -0300

    feat-exercicio1-paciente

--- src/entidades/Paciente.java
+++ src/entidades/Paciente.java
@@ -1,4 +1,17 @@
 package entidades;
 
+import java.util.List;
+
 public class Paciente {
+
+    //Exercício 1
+    public String nome;
+    public int idade;
+    public double peso;
+    public double altura;
+    public double pressaoArterial;
+    public double frequenciaCardiaca;
+    public String dietaAlimentar;
+    public List<String> atividadesFisicar;
+
 }
