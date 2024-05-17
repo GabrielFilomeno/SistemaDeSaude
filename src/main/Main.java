9741b06ead84ee1677540f48f8ddfcec3103c8f0
GabrielFilomeno <biel_floripa@hotmail.com>
2024-05-17 18:08:22 -0300

    feat-exercicio1

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.List;
+
+public class Main {
+
+    //Exercício 1
+    public String nome;
+    public int idade;
+    public double peso;
+    public double altura;
+    public double pressaoArterial;
+    public double frequenciaCardiaca;
+    public String dietaAlimentar;
+    public List<String> atividadesFisicar;
+
+}
