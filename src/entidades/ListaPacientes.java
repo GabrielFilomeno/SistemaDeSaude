d26f8406eb2ebac43e9f811828b743770d3c485e
GabrielFilomeno <biel_floripa@hotmail.com>
2024-05-20 15:17:07 -0300

    feat-exercicio3

--- src/entidades/ListaPacientes.java
+++ src/entidades/ListaPacientes.java
@@ -2,14 +2,86 @@ package entidades;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListaPacientes {
-
     private static List<Paciente> listaPacientes = new ArrayList<>();
 
-    public static List<Paciente> listarPacientes(){
-
+    public static List<Paciente> getListaPacientes() {
         return listaPacientes;
     }
+
+    public static void addPaciente(Paciente paciente) {
+        listaPacientes.add(paciente);
+    }
+
+    public static void removerPaciente(int id) {
+        listaPacientes.remove(id);
+    }
+
+    public static void listarPacientes() {
+        for(int i = 0; i < listaPacientes.size(); i++) {
+            System.out.println("ID: " + i + " - Nome: " + listaPacientes.get(i).getNome());
+        }
+    }
+
+    public static Paciente buscarPorId(int id) {
+        if (id >= 0 && id < listaPacientes.size()) {
+            return listaPacientes.get(id);
+        } else {
+            System.out.println("ID inválido. Por favor, tente novamente.");
+            return null;
+        }
+    }
+
+    public static void alterarDados(Scanner scan) {
+        System.out.println("Digite o ID do paciente que deseja alterar:");
+        int id = scan.nextInt();
+        Paciente paciente = buscarPorId(id);
+        if (paciente != null) {
+            System.out.println("Digite o novo nome:");
+            String nome = scan.next();
+            paciente.setNome(nome);
+
+            System.out.println("Digite a nova idade:");
+            int idade = scan.nextInt();
+            paciente.setIdade(idade);
+
+            System.out.println("Digite o novo peso:");
+            double peso = scan.nextDouble();
+            paciente.setPeso(peso);
+
+            System.out.println("Digite a nova altura:");
+            double altura = scan.nextDouble();
+            paciente.setAltura(altura);
+
+            System.out.println("Digite a nova pressão arterial:");
+            double pressaoArterial = scan.nextDouble();
+            paciente.setPressaoArterial(pressaoArterial);
+
+            System.out.println("Digite a nova frequência cardíaca:");
+            int frequenciaCardiaca = scan.nextInt();
+            paciente.setFrequenciaCardiaca(frequenciaCardiaca);
+
+            System.out.println("Digite a nova dieta alimentar:");
+            String dietaAlimentar = scan.next();
+            paciente.setDietaAlimentar(dietaAlimentar);
+
+            System.out.println("Quantas atividades físicas você deseja adicionar?");
+            int numAtividades = scan.nextInt();
+            List<String> atividadesFisicas = new ArrayList<>();
+            for (int i = 0; i < numAtividades; i++) {
+                System.out.println("Digite a atividade física " + (i + 1) + ":");
+                String atividade = scan.next();
+                atividadesFisicas.add(atividade);
+            }
+            paciente.setAtividadesFisicas(atividadesFisicas);
+
+            System.out.println("Dados do paciente atualizados com sucesso!");
+        } else {
+            System.out.println("Paciente não encontrado.");
+        }
+    }
+
 
 }
--- src/entidades/Paciente.java
+++ src/entidades/Paciente.java
@@ -1,17 +1,97 @@
 package entidades;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Paciente {
 
-    //Exercício 1
-    public String nome;
-    public int idade;
-    public double peso;
-    public double altura;
-    public double pressaoArterial;
-    public double frequenciaCardiaca;
-    public String dietaAlimentar;
-    public List<String> atividadesFisicar;
+    private String nome;
+    private int idade;
+    private double peso;
+    private double altura;
+    private double pressaoArterial;
+    private double frequenciaCardiaca;
+    private String dietaAlimentar;
+    private List<String> atividadesFisicas = new ArrayList<>();
 
+    public double calcularImc(){
+        double imc = peso / (altura * altura);
+        return imc;
+    }
+
+    public void monitoramentoPaciente() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        System.out.println("Altura: " + altura);
+        System.out.println("Pressão arterial: " + pressaoArterial);
+        System.out.println("Frequencia cardiaca: " + frequenciaCardiaca);
+        System.out.println("Dieta alimentar: " + dietaAlimentar);
+        System.out.println("Atividades fisica: " + atividadesFisicas);
+        System.out.println("IMC : " + calcularImc());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPressaoArterial() {
+        return pressaoArterial;
+    }
+
+    public double getFrequenciaCardiaca() {
+        return frequenciaCardiaca;
+    }
+
+    public String getDietaAlimentar() {
+        return dietaAlimentar;
+    }
+
+    public List<String> getAtividadesFisicas() {
+        return atividadesFisicas;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPressaoArterial(double pressaoArterial) {
+        this.pressaoArterial = pressaoArterial;
+    }
+
+    public void setFrequenciaCardiaca(double frequenciaCardiaca) {
+        this.frequenciaCardiaca = frequenciaCardiaca;
+    }
+
+    public void setDietaAlimentar(String dietaAlimentar) {
+        this.dietaAlimentar = dietaAlimentar;
+    }
+
+    public void setAtividadesFisicas(List<String> atividadesFisicas) {
+        this.atividadesFisicas = atividadesFisicas;
+    }
 }
